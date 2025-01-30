fd2abb16175a51d3608fa7e142b712420b7605d4
username <wecp-dummy@gmail.com>
2025-01-30 17:33:28 +0000

    day 19

--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -4,9 +4,18 @@ import { CommonModule } from "@angular/common";
 import { IplRoutingModule } from "./ipl-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { VoteComponent } from "./components/vote/vote.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MatchCreateComponent,
+    TeamCreateComponent,
+    CricketerCreateComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
