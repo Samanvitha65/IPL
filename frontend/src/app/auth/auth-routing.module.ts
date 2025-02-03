c288174110b9b2d5b3ccf187b3188f7a24c27946
username <wecp-dummy@gmail.com>
2025-02-03 04:18:34 +0000

    day22

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { RegistrationComponent } from "./components/registration/registration.component";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
